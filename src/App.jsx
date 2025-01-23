643d1ae1b86bdbfb3e2675b4df9faf95bd4ba0ac
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-01-22 19:07:06 -0500

    react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import Signup from "./views/Singup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Signup />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
