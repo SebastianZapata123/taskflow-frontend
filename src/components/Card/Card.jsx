7bc979a80a908bbe45b3808fd07d26daa0c8d2b3
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-01-24 01:06:03 -0500

    dndkit

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,32 @@
+import { MdAddTask } from "react-icons/md";
+import { RiDeleteBin6Line } from "react-icons/ri";
+
 import "./card.css";
+import NewTask from "../NewTask/NewTask";
 
 export default function Card() {
+  let color = "#FFB3BA";
   return (
     <>
-      <div className="card"></div>
+      <div
+        className="card"
+        style={{
+          borderTop: `10px solid ${color}`,
+        }}
+      >
+        <h3>Nombre del estado</h3>
+        <button className="removestatus">
+          <RiDeleteBin6Line />
+        </button>
+
+        <NewTask taskname="Hola" />
+        <NewTask taskname="Prueba de tareas" />
+
+        <button className="button-newtask">
+          <MdAddTask className="iconewtask" />
+          Add new task{" "}
+        </button>
+      </div>
     </>
   );
 }
---
+++ src/components/NewTask/NewTask.jsx
@@ -0,0 +1,13 @@
+import "./newtask.css";
+
+function NewTask(props) {
+  return (
+    <>
+      <div className="tasklabel">
+        <p className="taskname">{props.taskname}</p> <button type=""></button>
+      </div>
+    </>
+  );
+}
+
+export default NewTask;
