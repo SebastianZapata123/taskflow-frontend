d532445333cafa4239fa48d07d90724556f3c083
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-01-29 15:38:48 -0500

    data form task

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,14 +1,21 @@
 import "./menu.css";
 
 function Menu() {
+  function handleTaskForm(event) {
+    event.preventDefault();
+    const data = {
+      title: event.target.title.value,
+      desc: event.target.desc.value,
+    };
+  }
   return (
     <>
       <div className="formulario">
-        <form action="">
+        <form onSubmit={handleTaskForm}>
           <p>Task title</p>
-          <input type="text" />
+          <input type="text" name="title" />
           <p>Task description</p>
-          <input type="text" />
+          <input type="text" name="desc" />
           <button className="save" type="submit">
             Save
           </button>
