0cb28076b6c5f86c1ae4aab35b4940cdf0368be4
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-01-29 15:42:17 -0500

    url

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 import "./menu.css";
 
 function Menu() {
-  function handleTaskForm(event) {
+  async function handleTaskForm(event) {
     event.preventDefault();
     const data = {
       title: event.target.title.value,
@@ -14,22 +14,30 @@ function Menu() {
       },
       body: JSON.stringify(data),
     };
+
+    try {
+      const response = await fetch("http://localhost:3000/api/crear", options);
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+    return (
+      <>
+        <div className="formulario">
+          <form onSubmit={handleTaskForm}>
+            <p>Task title</p>
+            <input type="text" name="title" />
+            <p>Task description</p>
+            <input type="text" name="desc" />
+            <button className="save" type="submit">
+              Save
+            </button>
+          </form>
+        </div>
+      </>
+    );
   }
-  return (
-    <>
-      <div className="formulario">
-        <form onSubmit={handleTaskForm}>
-          <p>Task title</p>
-          <input type="text" name="title" />
-          <p>Task description</p>
-          <input type="text" name="desc" />
-          <button className="save" type="submit">
-            Save
-          </button>
-        </form>
-      </div>
-    </>
-  );
 }
 
 export default Menu;
